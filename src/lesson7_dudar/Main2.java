10a98ef1510516af14c8b511ba28813ae9b6162a
Vasyl Gladkyi <vasyl.gladky@gmail.com>
2022-06-07 17:52:10 +0300

    new lesson

---
+++ src/lesson7_dudar/Main2.java
@@ -0,0 +1,13 @@
+package lesson7_dudar;
+
+public class Main2 {
+    public static void main(String[] args) {
+        // цикл while
+        int i = 100 ;
+        while (i > 5) {
+            System.out.println(i);
+            i-=25;
+
+        }
+    }
+}
