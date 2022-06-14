61d90db01d7a5383b940a74060bbcbe8f245566c
Vasyl Gladkyi <vasyl.gladky@gmail.com>
2022-06-14 11:20:26 +0300

    charset update

--- src/Practice/Aplusb.java
+++ src/Practice/Aplusb.java
@@ -1,9 +1,18 @@
 package Practice;
 
-import java.io.FileReader;
+import java.io.*;
+import java.util.*;
+
+/*В единственной строке входного файла input.txt записаны два натуральных числа через пробел. Значения чисел не превышают 109.
+ В единственную строку выходного файла output.txt нужно вывести одно целое число — сумму чисел А и В.
+ */
 
 
 public class Aplusb {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        int s = 0;
+        String str;
+
     }
 }
--- src/Practice/MainTelephone.java
+++ src/Practice/MainTelephone.java
@@ -11,7 +11,7 @@ public class MainTelephone {
         calc[1]=1;
         calc[2]=20;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Skolko minut vi ispolzovali v etom mesyace?: ");
+        System.out.print("Сколько минут Вы использовали в этом месяце?: ");
         calc[3] = scanner.nextInt();
 
         double difference = calc[3] - calc[0];
@@ -19,9 +19,9 @@ public class MainTelephone {
         double overlimit = calc[0] * calc [1] + difference*calc[2];
 
         if (calc[3]>calc[0]){
-            System.out.println("Vi privisili limit na  " + difference + " minut, po etomu plata sostavit "+overlimit+" grn.");
+            System.out.println("Вы привысили лимит на  " + difference + " минут, по этому плата составит "+overlimit+" грн.");
         } else {
-            System.out.println("Plata sostavit " + inlimit+ " grn, soglasno tarifu");
+            System.out.println("Плата составит " + inlimit+ " грн, согласно Вашего тарифа");
         }
 
 
