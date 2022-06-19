6a24c5ba1c303b52c7f103e7d844522f6f4769e0
Vasyl Gladkyi <vasyl.gladky@gmail.com>
2022-06-19 14:47:19 +0300

    ООП

--- src/lesson12OOPClasses/Main.java
+++ src/lesson12OOPClasses/Main.java
@@ -3,24 +3,7 @@ package lesson12OOPClasses;
 public class Main {
     public static void main(String[] args) {
 
-        Transport vw = new Transport();
-        vw.setValues(210.5f,900,"Blue", new byte[] {0,0,0});
-        String res1 = vw.getValues();
-        System.out.println(res1);
-//        vw.speed = 210.5f;
-//        vw.weight = 900;
-//        vw.color = "Blue";
-//        vw.coordinate = new byte[] {0,0,0};
-
-        Transport truck = new Transport();
-        truck.speed = 140.5f;
-        truck.weight = 6700;
-        truck.color = "Black";
-        truck.coordinate = new byte[] {100,0,100};
-
-        String res2 = truck.getValues();
-        System.out.println(res2);
-
-
+        Transport vw = new Transport(210.5f, 900, "Blue", new byte[]{0, 0, 0});
+        Transport truck = new Transport(6700, new byte[]{100, 0, 100});
     }
 }
--- src/lesson12OOPClasses/Transport.java
+++ src/lesson12OOPClasses/Transport.java
@@ -5,16 +5,30 @@ public class Transport {
     // public - доступна отовсюду
     //protected - используеться в наследовании
     //private
-    public float speed;
-    public int weight;
-    public String color;
-    public byte[] coordinate;
+    private float speed;
+    private int weight;
+    private String color;
+    private byte[] coordinate;
 
-    public void setValues(float _speed, int _weight, String _color, byte[] _coordinate){
-        speed = _speed;
-        weight = _weight;
-        color = _color;
-        coordinate = _coordinate;
+    public Transport(float speed, int weight, String color, byte[] coordinate) {
+        // Join to DB
+        System.out.println("Konstruktor 1");
+        setValues(speed, weight, color, coordinate);
+        System.out.println(getValues());
+    }
+
+    public Transport(int weight,byte[] coordinate){
+        System.out.println("Konstruktor 2");
+        this.weight = weight;
+        this.coordinate = coordinate;
+        System.out.println(this.getValues());
+    }
+
+    public void setValues(float speed, int weight, String color, byte[] coordinate){
+        this.speed = speed;
+        this.weight = weight;
+        this.color = color;
+        this.coordinate = coordinate;
 
     }
 
